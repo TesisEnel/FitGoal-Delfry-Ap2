72649a013c1409972c9b439bc92f9f6a4fe7f1c1
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-18 01:10:10 -0400

    se agrega home screen

---
+++ app/src/main/java/edu/ucne/fitgoal/presentation/home/HomeScreen.kt
@@ -0,0 +1,16 @@
+package edu.ucne.fitgoal.presentation.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen() {
+    Text(
+        text = "Bienvenido a Home",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
