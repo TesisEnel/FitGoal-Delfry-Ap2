deda16f8ec66b1f412fe9dbf9286f83dd42230da
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-18 01:06:03 -0400

    se agrega nuevas pantallas y algunos colores, ademas de constantes

--- app/src/main/java/edu/ucne/fitgoal/ui/theme/Color.kt
+++ app/src/main/java/edu/ucne/fitgoal/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0xFF0497FD)
+val LightGray = Color(0xFFD9D6D9)
+
+val DarkGreen = Color(0xFF068251)
+val LightGreen = Color(0xFF85C485)
+val ExDarkGreen = Color(0xFF002B1A)
+val Error = Color(0xFFD32F2F)
---
+++ app/src/main/java/edu/ucne/fitgoal/util/Constants.kt
@@ -0,0 +1,7 @@
+package edu.ucne.fitgoal.util
+
+object Constants {
+    const val BASE_URL = "https://fitgoalapi.azurewebsites.net/"
+    const val API_KEY = "FitGoal"
+    const val ID_WEB_CLIENT = "1019711990584-87a572ke6ntg72h0op7id9gp1pl0n43m.apps.googleusercontent.com"
+}
