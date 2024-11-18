8863af75f96c8b687e39aba2afbfb9aecd498e42
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-18 01:11:45 -0400

    se agrega UiState

---
+++ app/src/main/java/edu/ucne/fitgoal/presentation/auth/UiState.kt
@@ -0,0 +1,32 @@
+package edu.ucne.fitgoal.presentation.auth
+
+import androidx.compose.ui.text.input.KeyboardType
+import edu.ucne.fitgoal.data.remote.dto.UsuarioDto
+
+data class UiState(
+    val nombre: String = "",
+    val nombreError: String = "",
+    val apellido: String = "",
+    val apellidoError: String = "",
+    val email: String = "",
+    val emailError: String = "",
+    val password: String = "",
+    val verifyPassword: String = "",
+    val passwordError: String = "",
+    val verifyPasswordError: String = "",
+    val isLoading: Boolean = false,
+    val error: String = "",
+    val canSeePassword: Boolean = false,
+    val keyboardType: KeyboardType = KeyboardType.Password,
+    val isAuthorized: Boolean = false,
+    var isEmailVerified: Boolean = false,
+    val isButtonEnabled: Boolean = true,
+    val tiempo: Int = 0
+)
+
+fun UiState.toDto() = UsuarioDto(
+    usuarioId = "",
+    nombre = nombre,
+    apellido = apellido,
+    correo = email
+)
