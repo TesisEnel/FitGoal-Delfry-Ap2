f74e91d422ecb41f57819905d2e0d5925fcfcb16
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-18 01:03:24 -0400

    se agrega navh host para main

--- app/src/main/java/edu/ucne/fitgoal/presentation/navigation/MainNavHost.kt
+++ app/src/main/java/edu/ucne/fitgoal/presentation/navigation/MainNavHost.kt
@@ -1,32 +1,31 @@
 package edu.ucne.fitgoal.presentation.navigation
 
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.composable
-import edu.ucne.fitgoal.presentation.planificador.PlanificadorScreen
-import edu.ucne.fitgoal.presentation.calculadora.CalculadoraScreen
-import edu.ucne.fitgoal.presentation.perfil.PerfilScreen
-import kotlinx.coroutines.launch
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.fitgoal.presentation.calculadora.CalculadoraScreen
+import edu.ucne.fitgoal.presentation.home.HomeScreen
+import edu.ucne.fitgoal.presentation.perfil.PerfilScreen
+import edu.ucne.fitgoal.presentation.planificador.PlanificadorScreen
 import edu.ucne.fitgoal.presentation.reloj.RelojScreen
 
 @Composable
-fun FitGoalNavHost(
-    navHostController: NavHostController
+fun MainNavHost(
+    navHostController: NavHostController = rememberNavController()
 ) {
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val selectedItem = remember { mutableStateOf("Home") }
 
-    Drawer(
-        drawerState = drawerState,
-        navHostController = navHostController
+    BoxBottomNav(
+        navHostController = navHostController,
+        selectedItem = selectedItem
     ) {
         NavHost(
             navController = navHostController,
-            startDestination = Screen.PlanificadorScreen
+            startDestination = Screen.HomeScreen,
         ) {
             composable<Screen.PlanificadorScreen> {
                 PlanificadorScreen(
@@ -37,9 +36,7 @@ fun FitGoalNavHost(
                         navHostController.navigate(Screen.RelojScreen)
                     },
                     onDrawer = {
-                        scope.launch {
-                            drawerState.open()
-                        }
+
                     }
                 )
             }
@@ -53,9 +50,7 @@ fun FitGoalNavHost(
                         navHostController.navigate(Screen.RelojScreen)
                     },
                     onDrawer = {
-                        scope.launch {
-                            drawerState.open()
-                        }
+
                     }
                 )
             }
@@ -68,13 +63,12 @@ fun FitGoalNavHost(
                         navHostController.navigate(Screen.PerfilScreen)
                     },
                     onDrawer = {
-                        scope.launch {
-                            drawerState.open()
-                        }
+
                     }
                 )
             }
             composable<Screen.CalculadoraScreen> {
+
                 CalculadoraScreen(
                     goToPlanificador = {
                         navHostController.navigate(Screen.PlanificadorScreen)
@@ -86,11 +80,16 @@ fun FitGoalNavHost(
                         navHostController.navigate(Screen.RelojScreen)
                     },
                     onDrawer = {
-                        scope.launch {
-                            drawerState.open()
-                        }
+
                     }
                 )
+            }
+            composable<Screen.HomeScreen> {
+                HomeScreen()
+            }
+
+            composable<Screen.AuthNavHostScreen> {
+                AuthNavHost()
             }
         }
     }
