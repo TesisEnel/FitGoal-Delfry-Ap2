c92d6411a7c892a8c1cf84dbc0747403926688e5
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-18 01:08:18 -0400

    se agrega metodos de la api para Usuarios en RemoteDataSource

--- app/src/main/java/edu/ucne/fitgoal/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/fitgoal/data/remote/RemoteDataSource.kt
@@ -1,9 +1,16 @@
 package edu.ucne.fitgoal.data.remote
 
-class RemoteDataSource(
+import edu.ucne.fitgoal.data.remote.dto.UsuarioDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
     private val fitGoalApi: FitGoalApi
 ) {
     suspend fun getPerfil() = fitGoalApi.getPerfil()
     suspend fun getReloj() = fitGoalApi.getReloj()
     suspend fun getPlanificador() = fitGoalApi.getPlanificador()
+
+    suspend fun postUsuario(usuarioDto: UsuarioDto) = fitGoalApi.postUsuario(usuarioDto)
+    suspend fun putUsuario(id: String, usuarioDto: UsuarioDto) = fitGoalApi.putUsuario(id, usuarioDto)
+    suspend fun getUsuario(id: String) = fitGoalApi.getUsuario(id)
 }
