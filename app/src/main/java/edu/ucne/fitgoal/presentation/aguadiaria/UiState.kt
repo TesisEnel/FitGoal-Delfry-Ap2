ea8bae9e8ddf2a34741f69906d9475c18d9e064c
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-27 16:53:10 -0400

    Se agrega UiState y Event de HorarioBebida

---
+++ app/src/main/java/edu/ucne/fitgoal/presentation/aguadiaria/UiState.kt
@@ -0,0 +1,25 @@
+package edu.ucne.fitgoal.presentation.aguadiaria
+
+import edu.ucne.fitgoal.data.local.entities.HorarioBebidaEntity
+import edu.ucne.fitgoal.data.remote.dto.HorarioBebidaDto
+
+data class UiState(
+    val isLoading: Boolean = false,
+    val error: String = "",
+    val isModalVisible : Boolean = false,
+    val horarios : List<HorarioBebidaEntity> = emptyList(),
+    val cantidad: Float = 0f,
+    val cantidadError: String = "",
+    val hora: String = "",
+    val horaError: String = "",
+    val horarioBebidaId: Int = 0,
+    val usuarioId: String = "",
+    val update: Boolean = false
+)
+
+fun UiState.toDto() = HorarioBebidaDto(
+    horarioBebidaId = horarioBebidaId,
+    cantidad = cantidad,
+    hora = hora,
+    usuarioId = usuarioId
+)
