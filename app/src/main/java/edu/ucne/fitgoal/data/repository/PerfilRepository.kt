50206422b6e6c5ffecf1e8eb215aac76e1887566
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-11-27 14:13:07 -0400

    Modifica funcion de PerfilRepository

--- app/src/main/java/edu/ucne/fitgoal/data/repository/PerfilRepository.kt
+++ app/src/main/java/edu/ucne/fitgoal/data/repository/PerfilRepository.kt
@@ -2,25 +2,25 @@ package edu.ucne.fitgoal.data.repository
 
 import edu.ucne.fitgoal.data.remote.RemoteDataSource
 import edu.ucne.fitgoal.data.remote.Resource
-import edu.ucne.fitgoal.data.remote.dto.PerfilDto
+import edu.ucne.fitgoal.data.remote.dto.UsuarioDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import javax.inject.Inject
 import retrofit2.HttpException
+import javax.inject.Inject
 
 class PerfilRepository @Inject constructor (
     private val remoteDataSource: RemoteDataSource
 ){
-    fun getPerfil(): Flow<Resource<List<PerfilDto>>> = flow {
+    fun getUsuario(id: String): Flow<Resource<UsuarioDto>> = flow {
         try {
             emit(Resource.Loading())
-            val clientes = remoteDataSource.getPerfil()
-            emit(Resource.Success(clientes))
+            val usuario = remoteDataSource.getUsuario(id)
+            emit(Resource.Success(usuario))
         } catch (e: HttpException) {
             val errorMessage = e.response()?.errorBody()?.string() ?: e.message()
-            emit(Resource.Error("Error de conexion $errorMessage"))
+            emit(Resource.Error("Error de conexión: $errorMessage"))
         } catch (e: Exception) {
-            emit(Resource.Error("Error ${e.message}"))
+            emit(Resource.Error("Error: ${e.message}"))
         }
     }
 }
