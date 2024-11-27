30839c2aebb48011d59ff6e680c3685ad13499ef
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-27 16:47:09 -0400

    Se agrega dependencias para timepicker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.9"
@@ -137,4 +138,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
     implementation(libs.coil.gif)
+
+    implementation (libs.datetime)
+    coreLibraryDesugaring (libs.desugar.jdk.libs)
 }
