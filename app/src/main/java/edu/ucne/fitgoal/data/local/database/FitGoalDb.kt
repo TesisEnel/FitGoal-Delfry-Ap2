6c5a49db1809103433d8291d9b8044adc825a3fc
ElPelfry <delfry_paulino@ucne.edu.do>
2024-11-27 16:49:49 -0400

    Se agrega HorarioBebida, entity,Dao,Db

--- app/src/main/java/edu/ucne/fitgoal/data/local/database/FitGoalDb.kt
+++ app/src/main/java/edu/ucne/fitgoal/data/local/database/FitGoalDb.kt
@@ -4,22 +4,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.fitgoal.data.local.dao.EjercicioDao
 import edu.ucne.fitgoal.data.local.dao.FitGoalDao
+import edu.ucne.fitgoal.data.local.dao.HorarioBebidaDao
 import edu.ucne.fitgoal.data.local.dao.UsuarioDao
 import edu.ucne.fitgoal.data.local.entities.EjercicioEntity
 import edu.ucne.fitgoal.data.local.entities.FitGoalEntity
+import edu.ucne.fitgoal.data.local.entities.HorarioBebidaEntity
 import edu.ucne.fitgoal.data.local.entities.UsuarioEntity
 
 @Database(
     entities = [
         FitGoalEntity::class,
         UsuarioEntity::class,
-        EjercicioEntity::class
+        EjercicioEntity::class,
+        HorarioBebidaEntity::class
                ],
-    version = 3,
+    version = 5,
     exportSchema = false
 )
 abstract class FitGoalDb : RoomDatabase(){
     abstract fun fitGoalDao(): FitGoalDao
     abstract fun usuarioDao(): UsuarioDao
     abstract fun ejercicioDao(): EjercicioDao
+    abstract fun horarioBebidaDao(): HorarioBebidaDao
 }
