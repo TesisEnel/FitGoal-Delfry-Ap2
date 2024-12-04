40766e90da8eba6db4f1d2eaf7675d0f1d017915
ElPelfry <delfry_paulino@ucne.edu.do>
2024-12-04 05:58:44 -0400

    agrega nuevos eventos

--- app/src/main/java/edu/ucne/fitgoal/presentation/home/HomeEvent.kt
+++ app/src/main/java/edu/ucne/fitgoal/presentation/home/HomeEvent.kt
@@ -1,6 +1,13 @@
 package edu.ucne.fitgoal.presentation.home
 
 sealed class HomeEvent {
-    data object FetchTips : HomeEvent()
+    data object UpdateProgreso : HomeEvent()
     data object CloseErrorModal : HomeEvent()
+    data object CloseModal : HomeEvent()
+    data object OpenRegistrarProgreso : HomeEvent()
+    data object OpenNuevaMeta : HomeEvent()
+    data object SaveProgreso : HomeEvent()
+    data class PesoChanged(val peso: String) : HomeEvent()
+    data class PesoIdealChanged(val pesoIdeal: String) : HomeEvent()
+    data class PesoInicialChanged(val pesoInicial: String) : HomeEvent()
 }
