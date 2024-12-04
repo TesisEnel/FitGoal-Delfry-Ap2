14f8e024989bc72cd802cd6f7fd64858ddad7052
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-12-03 21:24:31 -0400

    Elimina eventos innecesarios

--- app/src/main/java/edu/ucne/fitgoal/presentation/perfil/PerfilEvent.kt
+++ app/src/main/java/edu/ucne/fitgoal/presentation/perfil/PerfilEvent.kt
@@ -3,9 +3,7 @@ package edu.ucne.fitgoal.presentation.perfil
 sealed interface PerfilEvent {
     data object GetPerfil : PerfilEvent
     data object NavigateToEditarPerfil : PerfilEvent
-    data object NavigateToCalculadora : PerfilEvent
-    data object NavigateToTips : PerfilEvent
-    data object NavigateToAyuda : PerfilEvent
+    data object NavigateToProgresoDiario : PerfilEvent
     data object Logout : PerfilEvent
     data object CloseErrorModal : PerfilEvent
 }
